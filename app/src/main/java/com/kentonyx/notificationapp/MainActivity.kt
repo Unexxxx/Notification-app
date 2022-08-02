1f8c3fb73ded59be788c5a64b0e08583395691e9
Kent Onyx <kosarintok@unionbankph.com>
2022-08-02 15:57:08 +0800

    Add navigation menu

--- app/src/main/java/com/kentonyx/notificationapp/MainActivity.kt
+++ app/src/main/java/com/kentonyx/notificationapp/MainActivity.kt
@@ -2,10 +2,43 @@ package com.kentonyx.notificationapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import com.kentonyx.notificationapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+    lateinit var toggle: ActionBarDrawerToggle
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.apply {
+            toggle = ActionBarDrawerToggle(this@MainActivity,dlActionbar,R.string.open,R.string.close)
+            dlActionbar.addDrawerListener(toggle)
+            toggle.syncState()
+
+            supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+            nvMenu.setNavigationItemSelectedListener {
+                when(it.itemId){
+                    R.id.settings->{Toast.makeText(this@MainActivity,"Settings",Toast.LENGTH_SHORT).show()}
+                    R.id.termsAndConditions->{Toast.makeText(this@MainActivity,"Terms and Conditions",Toast.LENGTH_SHORT).show()}
+                    R.id.dataPolicy->{Toast.makeText(this@MainActivity,"Data Policy",Toast.LENGTH_SHORT).show()}
+                    R.id.logOut->{Toast.makeText(this@MainActivity,"Log out",Toast.LENGTH_SHORT).show()}
+                }
+                true
+            }
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(toggle.onOptionsItemSelected(item)){
+            true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
